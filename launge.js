1bd6b1299ccea1aed7d732b4aa79e8c56d7777d0
kaguya.ootsutsukii <61389918+Hepka@users.noreply.github.com>
2021-09-12 23:47:17 +0400

    update launge.js

--- launge.js
+++ launge.js
@@ -1,22 +1,13 @@
-// setup data
-const messages = {}
-
-messages.deleteAlert = `{user}, invite links are not allowed!`
-
-messages.logging.embed.title = `Invite link was posted`
-messages.logging.embed.field1.name = `User:`
-messages.logging.embed.field1.value = `{user}`
-messages.logging.embed.field2.name = `Channel:`
-messages.logging.embed.field2.value = `{channel}`
-messages.logging.embed.field3.name = `Content:`
-messages.logging.embed.field3.value = `{content}`
-messages.logging.embed.footer.text = `AntiInviteLinkDiscord`
-messages.logging.embed.footer.imageEnabled = true
-messages.logging.embed.footer.image = `{botAvatar}`
-messages.logging.embed.timestamp = true
-messages.logging.embed.color = `#ffffff`
-
-messages.logging.noEmbed = `User:\n{user}\nChannel:\n{channel}\nContent:\n{content}`
-
-// export data
-export { messages }
+export default const messages = {
+	alert: "{{ USER_MENTION }}, the invitation link is prohibited on this server!", // This message will be displayed when someone posts an invitation link
+	logEmbed: {
+		title: "Invite link was posted", 
+		description: "Sender: {{ USER }}\nChannel: {{ CHANNEL }}\nMessage Content: {{ CONTENT }}",
+		color: "#123456", // The color of the strip on the left in the "hex" format
+		footer: {
+			text: "AntiInviteLink", // You can write anything here. This will be displayed at the bottom of the embed.
+			iconURL: "serverAvatar" // Possible values: botAvatar, senderAvatar, serverAvatar
+		},
+		timestamp: Date.now() // Put "//" in front of this line, if you don't need time in embed.
+	}
+}
